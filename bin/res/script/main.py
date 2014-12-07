1391a4d32483f0e1a0060a9ce192b4578e64adf8
youlanhai <you_lan_hai@foxmail.com>
2014-12-07 22:49:57 +0800

    script

--- bin/res/script/main.py
+++ bin/res/script/main.py
@@ -2,8 +2,9 @@
 
 import traceback
 
+import helper
 import lui
-import ui
+import Lazy
 
 from share import share
 
@@ -12,61 +13,9 @@ def init() :
 	print("game init.")
 	print("lui:", dir(lui))
 	
-	try :
-		import gui
-		lui.regUIFactory(gui.createUIFromStream)
-
-		import guimgr
-		share.gui = guimgr.guimgr()
-
-		import Lazy
-		from avatar import Avatar
-
-		playerID = Lazy.createEntity(Avatar)
-		player = Lazy.entity(playerID)
-		Lazy.setPlayer(player)
-		
-		sky = Lazy.getSkyBox()
-		size = 200
-		sky.setRange((-size, -size, -size), (size, size, size))
-		sky.source = player
-		sky.image = "gui/sky/01/%d.jpg"
-
-		if True:
-			Lazy.map().loadMap("map/test")
-		else:
-			Lazy.map().createMap("map/abc", 5, 5)
-			Lazy.map().saveMap("map/abc")
-
-	except:
-		print(traceback.print_exc())
-		
 	return True
 
 
 def fini() :
-	try :
-		
-		share.guimgr = None
-		lui.root().clearChildren()
-		
-	except:
-		print(traceback.print_exc())
-
 	print ("game fini.")
 
-
-
-#窗口改变大小
-def onSize(w, h):
-	print("python onSize", w, h)
-	ui.onSize(w, h)
-	share.gui.editor.setSize(w, h)
-
-def onClientSize(w, h):
-	ui.onClientSize(w, h)
-
-def onKeyEvent(isDown, key):
-	#print("onKeyEvent", isDown, key)
-	
-	return share.gui.onKeyEvent(isDown, key)
